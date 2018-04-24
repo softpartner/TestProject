00c10baef7740b6c2e8e54f32a7bf5e0cc8efc3b
Abdellatif <abdellatif1@gmail.com>
2018-04-24 21:37:51 +0000

    test3

--- HelloWord.java
+++ HelloWord.java
@@ -2,7 +2,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<20;i++) {
+		for(int i=0;i<6;i++) {
 			System.out.println("...........................HelloWord .......................");
 		}
 		
