b5800b910965a8f29006eaa7deb3e45a80fcd8fe
Abdellatif <abdellatif1@gmail.com>
2018-04-25 18:03:30 +0000

    nbre iteration = 4

--- HelloWord.java
+++ HelloWord.java
@@ -1,11 +1,8 @@
 public class HelloWord {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<6;i++) {
+		for(int i=0;i<4;i++) {
 			System.out.println("...........................HelloWord .......................");
 		}
-		
 	}
-
 }
